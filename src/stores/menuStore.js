188d246122213fdd8cbdd724ffdf2769a199c0dd
wardi <alifashifan@gmail.com>
2025-07-08 19:09:27 +0800

    sub menu done, next layouting

--- src/stores/menuStore.js
+++ src/stores/menuStore.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { sidebarMenus } from './sidebarMenus'
+import { sidebarMenus } from './sidebarMenu'
 
 export const useMenuStore = defineStore('menu', {
   state: () => ({
